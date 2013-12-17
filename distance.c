6098c59cde51d8d28be93b9a670c822ee8e9d077
Henrik Holst <holst@matmech.com>
2013-12-17 23:31:09 +0100

    Changed definition of "distance" from bytes to floats and doubles in two separate routines.

--- distance.c
+++ distance.c
@@ -1,7 +1,12 @@
-/* vi:ft=c:et:ts=2:sw=2 */
+/* vim:set ft=c et ts=2 sw=2: */
 #include <stddef.h>
 
-ptrdiff_t distance(char *a, char *b)
+ptrdiff_t distance_float(const float *a, const float *b)
+{
+  return b - a;
+}
+
+ptrdiff_t distance_double(const double *a, const double *b)
 {
   return b - a;
 }
